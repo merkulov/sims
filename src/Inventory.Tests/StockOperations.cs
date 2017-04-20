6a4229898e4d3da1776021b484deb42bd6b401b0
Mikhail Merkulov <merkulov@corepartners.com>
2017-04-20 13:41:27 -0400

    Infrastructure + test for creating stock.

---
+++ src/Inventory.Tests/StockOperations.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+using System.Linq;
+using Inventory.Model;
+using Marten;
+using NUnit.Framework;
+
+namespace Inventory.Tests
+{
+	[TestFixture]
+	public class StockOperations
+	{
+		[Test]
+		public void CanAddStock()
+		{
+			var store = DocumentStore.For(
+				options => {
+					options.Connection(ConfigurationManager.ConnectionStrings["main"].ConnectionString);
+				}
+			);
+
+			var stock = new Stock();
+			stock.Name = "123";
+			using (var session = store.OpenSession())
+			{
+				session.Store(stock);
+
+				session.SaveChanges();
+			}
+
+			using (var session = store.QuerySession())
+			{
+				var stockFromDb = session.Query<Stock>().FirstOrDefault(s => s.Name == "123");
+				Assert.IsNotNull(stockFromDb);
+				Assert.AreEqual("123", stockFromDb.Name);
+			}
+		}
+	}
+}
---
+++ src/Inventory/Model/Stock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Inventory.Model
+{
+	public class Stock
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+	}
+}
